32e973ae5becf68c59a9008fb85ca6f69233940f
zengxiaosen <919451814@qq.com>
2017-12-06 10:12:06 +0800

    test

---
+++ Test/stl/d2/stack.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+//stack容器基本操作
+void test01(){
+	
+	stack<int> mystack;
+	mystack.push(8);
+	mystack.push(2);
+	mystack.push(7);
+	mystack.push(0);
+
+	stack<int> mystack2(mystack); //拷贝构造
+
+	if (mystack.empty()){
+		cout << "空!" << endl;
+	}
+	else{
+		cout << "不空！" << endl;
+	}
+
+
+	while (mystack.size() > 0 ){
+		int val = mystack.top(); // top返回栈顶元素
+		cout << val << " ";
+		mystack.pop();
+	}
+	cout << endl;
+
+}
+
+class Teacher{
+	public:
+		Teacher(string name, int age) :name(name), age(age){
+
+		}
+		string name;
+		int age;
+};
+
+void test02(){
+	stack<Teacher> mystack;
+	Teacher t1("aaa", 10), t2("bbb", 20), t3("ccc", 30);
+
+	mystack.push(t1);
+	mystack.push(t2);
+	mystack.push(t3);
+
+	while (mystack.size() > 0){
+		Teacher t = mystack.top();
+		cout << "name: " << t.name << " age: " << t.age << endl;
+		mystack.pop();
+	}
+}
+
+void test03(){
+	stack<Teacher*> mystack;
+	Teacher t1("aaa", 10), t2("bbb", 20), t3("ccc", 30);
+
+	Teacher* p1 = &t1;
+	Teacher* p2 = &t2;
+	Teacher* p3 = &t3;
+
+	mystack.push(p1);
+	mystack.push(p2);
+	mystack.push(p3);
+
+	
+	while (mystack.size() > 0){
+		Teacher* p =  mystack.top();
+		cout << "name:" << p->name << " age" << p->age << endl;
+		mystack.pop();
+	}
+	cout << endl;
+}
+
+int main(){
+	test03();
+	return 0;
+	
+}
