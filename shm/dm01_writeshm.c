d7c8fb19d6f3fbb0573f73925b86e740928308e4
zengxiaosen <919451814@qq.com>
2017-12-09 00:08:49 +0800

    test

---
+++ shm/dm01_writeshm.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+void main(){
+	printf("hello\n");
+}
