63bc0ec0530844943f2b915c5e973969c8c2bc8b
zengxiaosen <919451814@qq.com>
2017-12-09 12:40:38 +0800

    test

--- shm/dm01_writeshm.c
+++ shm/dm01_writeshm.c
@@ -14,6 +14,52 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-void main(){
-	printf("hello\n");
+#define ERR_EXIT(m) \
+		do \
+		{ \
+				perror(m); \ 
+				exit(EXIT_FAILURE); \
+		} while(0)
+
+typedef struct _Teacher{
+	char name[64];
+	int age;
+} Teacher;
+
+int main(){
+	int ret = 0;
+	int shmid;
+	shmid = shmget(0x2232, sizeof(Teacher), 0666 | IPC_CREAT);
+	if(shmid == -1){
+		perror("shmget err ..2222.");
+		if(shmid < 0 && errno==ENOENT){
+			printf("check share memory not exit\n");
+		}
+		printf("shmid errno: %d\n", shmid);
+		return errno;
+	}
+	Teacher *p = NULL;
+	p = shmat(shmid, NULL, 0);
+	if(p == (void *)-1){
+		perror("shmget err");
+		return errno;
+	}
+
+	strcpy(p->name, "aaaa");
+	p->age = 33;
+
+	printf("shut down the share memory, other not delete \n");
+
+	int num;
+	scanf("%d", &num);
+
+	if(num == 1){
+		// drop the link to share memory
+		shmdt(p);
+		ret = shmctl(shmid, IPC_RMID, NULL);
+		if(ret < 0){
+			perror("rm errr\n");
+		}
+	}
+	return 0;
 }
