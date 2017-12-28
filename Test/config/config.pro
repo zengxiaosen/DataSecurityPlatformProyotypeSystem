e305b9a3cf97ffa5c5655c7924bbe0702c359060
zengxiaosen <919451814@qq.com>
2017-12-28 22:02:05 +0800

    test

---
+++ Test/config/config.pro
@@ -0,0 +1,12 @@
+QT += core network widgets
+QT -= gui
+
+CONFIG += c++11
+
+TARGET = config
+CONFIG += console
+CONFIG -= app_bundle
+
+TEMPLATE = app
+
+SOURCES += main.cpp
---
+++ Test/config/main.cpp
@@ -0,0 +1,30 @@
+#include <QCoreApplication>
+#include <QSettings>
+#include <QDebug>
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    {
+        QSettings settings("config.conf", QSettings::InitFormat);
+        settings.beginGroup("server");
+        settings.setValue("port", 80);
+        settings.setValue("threads", 4);
+        settings.setValue("log_level", "debug");
+        settings.endGroup();
+
+        settings.setValue("server/max_connection", 1024);
+        qDebug()  << "create configuration end" << endl;
+
+
+    }
+
+    {
+        QSettings conf("config.conf", QSettings::IniFormat);
+        qDebug() << "server port: " << conf.value("server/port").toInt();
+        conf.beginGroup("server");
+        qDebug() << "server threads: " << conf.value("threads");
+        qDebug() << "server log level: " << conf.value("log_level");
+        conf.endGroup();
+    }
+    return a.exec();
+}
